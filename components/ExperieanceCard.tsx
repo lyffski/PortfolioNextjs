d8f7a066f0c7c804b3cd395b7eb9d7425fdafd44
layt <sobczak.sk@protonmail.ch>
2023-04-25 14:06:38 +0200

    new

--- components/ExperieanceCard.tsx
+++ components/ExperieanceCard.tsx
@@ -10,7 +10,6 @@ type Props = {
 
 
 const ExperieanceCard = ({experience}: Props) => {
-  const dated = new Date(experience.dateEnded).toDateString
   return (
     
     <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
