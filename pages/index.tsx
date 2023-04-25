bb5848b045cb6fa135126fbaa81ec733c3e4409d
layt <sobczak.sk@protonmail.ch>
2023-04-25 13:53:40 +0200

    remoed dea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type {GetStaticProps, NextPage} from "next"
+import type {GetStaticProps} from "next"
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About"
@@ -6,7 +6,6 @@ import WorkExperience from "../components/WorkExperience"
 import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 import ContactMe from "../components/ContactMe"
-import Link from "next/link";
 import { Experience, PageInfo, Project, Skill, Social } from "../typings"
 import {fetchSkills} from "../utils/fetchSkills";
 import {fetchSocials} from "../utils/fetchSocials";
